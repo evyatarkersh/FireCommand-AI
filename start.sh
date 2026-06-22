15569b54671914389e46aa9c509697d9c38dd1dd
Evyatar Kersh <evyatar.kersh@sap.com>
2026-06-22 15:32:31 +0300

    translate start.sh comments

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ echo "🏗️ Initializing Database..."
 python init_db.py
 
 echo "🚀 Starting Background Worker..."
-# ה-& בסוף מריץ את הפעולה ברקע בלי לחסום את שאר הקוד
+# The & at the end runs the operation in the background without blocking the rest of the code
 python worker.py &
 
 echo "🌐 Starting Web Server..."
-# מריץ את Gunicorn עבור שרת ה-Web
+# Runs Gunicorn for the Web server
 gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 run:app
